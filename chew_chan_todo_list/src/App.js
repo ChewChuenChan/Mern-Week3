52db90ef007bf644869d828df6aa95bb75c9b7d9
Chew Chuen Chan <aliceccc@gmail.com>
2022-07-25 01:02:26 -0400

    fixed the mistakes

--- chew_chan_todo_list/src/App.js
+++ chew_chan_todo_list/src/App.js
@@ -1,18 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import List from './components/List';
 
 
 function App() {
 
   const [todoArr, setToDoArr] = useState([]);
-  
-
 
   return (
     <div className="App">
       <h1>To Do List</h1>
       <Form todoArr={todoArr} setToDoArr={setToDoArr} />
+      <List todoArr={todoArr} setToDoArr={setToDoArr}/>
 
     </div>
   );
--- chew_chan_todo_list/src/components/List.js
+++ chew_chan_todo_list/src/components/List.js
@@ -1,11 +1,45 @@
-import { useState } from 'react';
 
-const List = (props) =>{
-    const {todoArr,setToDoArr} = props;
+const List = (props) => {
 
+    const {todoArr, setToDoArr} = props;
+
+    const completedHandler = (todos) =>{
+        todos.completed = !todos.completed;
+        setToDoArr([...todoArr]);
+    };
+
+    const deleteHandler = (delindex) =>{
+        const filteredTodos = todoArr.filter ((todos,index) =>{
+            return todos.id !== delindex;
+        });
+        setToDoArr(filteredTodos);
+    };
+
+    const styled = (completed) => {
+        if (completed === true){
+            return "completed";
+        }
+        else{
+            return "notcompleted";
+        }
+        };
 
     return(
-
+        <div className='card text-center m-3'>
+        {
+            todoArr.map((todos,id) =>{
+            return (
+                <div className ={styled(todos.completed)} key ={todos.id}>
+                    <span className="m-2">{todos.text}</span>
+                    <input type="checkbox" classame="btn-check" autoComplete="off" onChange={(e) => completedHandler(todos)}/>
+                    <button onClick={(e) => deleteHandler(todos.id)} className="btn btn-danger m-3">Delete</button>
+                </div>
+                );
+            })
+        }
+        </div>
     );
-}
+
+};
+
 export default List;
